f14f39ae5d019dccc00d9b6d2efa2eb00db3cd84
Giomar Santiago <giomar.santiago@upr.edu>
2022-04-02 21:18:33 -0400

    Added ability to register new user

---
+++ Fant4sticProject/controller/user.py
@@ -0,0 +1,40 @@
+from config.databaseConnect import DatabaseConnect
+from dao.user import UserDAO
+from flask import jsonify, request
+
+class UserController:
+    def __init__(self):
+        self.connection = DatabaseConnect().getConnection()
+
+    def registerNewUser(self, json):
+        dao = UserDAO()
+        first_name = json['FirstName']
+        last_name = json['LastName']
+        user_name = json['Username']
+        email = json['Email']
+
+        record = dao.existUser(email)
+        exist = record[0]
+
+        if exist:
+            return jsonify("This email is already registered.")
+
+        password = json['Password']
+        age = json['Age']
+        sex = json['Sex']
+        phone_num = json['PhoneNumber']
+        userType = json['UserType']
+        role_id = dao.getRoleID(userType)
+
+        if userType == 'Admin':
+            user_id = dao.registerNewUser(role_id, first_name, last_name, user_name, email, password, age, sex, phone_num)
+            json['User_id'] = user_id
+
+        elif userType == 'Customer':
+            user_id = dao.registerNewUser(role_id, first_name, last_name, user_name, email, password, age, sex, phone_num)
+            json['User_id'] = user_id
+
+        else:
+            return jsonify("Invalid user type."), 400
+
+        return jsonify(json), 201
