5b3afb23b3868cdd8ceedcca9162aabad5cbe856
Giomar Santiago <giomar.santiago@upr.edu>
2022-05-03 01:03:22 -0400

    Testing Axios GET method

--- frontend/src/CustomerView.js
+++ frontend/src/CustomerView.js
@@ -1,12 +1,35 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import {Button, Card, Container, Divider, Header, Icon, Modal, Tab} from "semantic-ui-react";
 import Dashboard from "./Dashboard";
 import Products from "./Products";
-
+import Axios from "axios";
 
 function CustomerView(){
     const [isAuth, setIsAuth] = useState(true)
     const [notShow, setNotShow] = useState(false)
+    const [data, setDate] = useState([])
+
+    useEffect(() => {
+        Axios.get('https://fant4stic-books.herokuapp.com/fant4stic/user/get_all')
+            .then(res => {
+                console.log("Getting from ::::", res.data)
+                setDate(res.data)
+            }).catch(err => console.log(err))
+    }, [])
+
+    const arr = data.map((data, index) => {
+        return (
+            <tr>
+               <td>{data.FirstName}</td>
+                <td>{data.LastName}</td>
+                <td>{data.UserName}</td>
+                <td>{data.Email}</td>
+                <td>{data.Age}</td>
+                <td>{data.Sex}</td>
+                <td>{data.PhoneNumber}</td>
+            </tr>
+        )
+    })
     const panes = [
         {
             menuItem: 'Products', render: () => (
@@ -34,7 +57,21 @@ function CustomerView(){
                     <Header as='h2' color='red'>
                         <Icon name='address card'/>Customer's Profile
                     </Header>
+                    <table>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Age</th>
+                            <th>Sex</th>
+                            <th>Phone number</th>
+                        </tr>
+                        {arr}
+
+                    </table>
                 </Tab.Pane>
+
             )
         },
         {
